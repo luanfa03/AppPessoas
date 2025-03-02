5e86e7c681d38d6952752ff5f9264f69c1b8b36b
Luan Fonseca <127336917+luanfa03@users.noreply.github.com>
2025-03-02 11:57:33 -0300

    Create CorsConfig.java

---
+++ AppPessoas/src/main/java/br/com/lfa/AppPessoas/config/CorsConfig.java
@@ -0,0 +1,21 @@
+package br.com.lfa.AppPessoas.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Permite todas as rotas
+                .allowedOrigins("http://localhost:4200") // Permite chamadas do frontend
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos permitidos
+                .allowedHeaders("*") // Permite todos os cabeçalhos
+                .allowCredentials(true);
+    }
+}
